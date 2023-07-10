fb390b6162caa3086b7101f6be593f38c187d889
BoroDinS <sergey.borodin.lg@gmail.com>
2023-07-10 14:44:37 +0200

    make customFilter working getting values from select options and append them to search params

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Hero, SearchBar, CustomFilter, CarCard } from "@/components";
+import { fuels, yearsOfProduction } from "@/constants";
 import { fetchCars } from "@/utils";
 
 export default async function Home({ searchParams }: any) {
@@ -23,8 +24,8 @@ export default async function Home({ searchParams }: any) {
         <div className="home__filters">
           <SearchBar />
           <div className="home__filter-container">
-            <CustomFilter title="fuel" />
-            <CustomFilter title="year" />
+            <CustomFilter title="fuel" options={fuels} />
+            <CustomFilter title="year" options={yearsOfProduction} />
           </div>
         </div>
         {!isDataEmpty ? (
